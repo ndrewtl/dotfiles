e134adfe6d75e13990b29e88a62c194d239fcdb9
Andrew Lee <andrewl@mbda.fun>
2019-04-06 21:30:43 -0700

    Abstract out shell bootstrapping

---
+++ sh/bootstrap.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Bootstrap: source all the other important shell startup commands in this
+# directory
+source $XDG_CONFIG_HOME/sh/applications.sh
+source $XDG_CONFIG_HOME/sh/path.sh
+source $XDG_CONFIG_HOME/sh/aliases.sh
+source $XDG_CONFIG_HOME/sh/kb.sh
--- zsh/.zshenv
+++ zsh/.zshenv
@@ -1,5 +1,1 @@
-source $XDG_CONFIG_HOME/sh/applications.sh
-source $XDG_CONFIG_HOME/sh/path.sh
-source $XDG_CONFIG_HOME/sh/aliases.sh
-source $XDG_CONFIG_HOME/sh/kb.sh
 source $XDG_CONFIG_HOME/zsh/prompt.sh
