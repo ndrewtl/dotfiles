5d9369ac7198129a9fd1803cf05359edc10801b3
Andrew Lee <andrewl@mbda.fun>
2019-04-06 21:58:03 -0700

    Move XDG_CONFIG_HOME setting into shell bootstraps

--- bash/bootstrap.sh
+++ bash/bootstrap.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+export XDG_CONFIG_HOME=$HOME/.config
 source $XDG_CONFIG_HOME/sh/bootstrap.sh  # general shell setup
 source $XDG_CONFIG_HOME/bash/prompt.sh   # set bash prompt
--- zsh/bootstrap.sh
+++ zsh/bootstrap.sh
@@ -1,4 +1,5 @@
 #!/bin/zsh
 
+export XDG_CONFIG_HOME=$HOME/.config
 source $XDG_CONFIG_HOME/sh/bootstrap.sh # general shell setup
 source $XDG_CONFIG_HOME/zsh/prompt.sh   # set zsh prompt
