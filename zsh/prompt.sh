2b60a093c00bd8e1201ac49d20ce86297a0035e9
Andrew Lee <andrewl@mbda.fun>
2019-04-06 22:05:48 -0700

    Clean up zsh prompt

--- zsh/prompt.sh
+++ zsh/prompt.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 # this prompt is for zsh only
 
-export PS1=$'%{\e[0;97;49m%}%n%{\e[0m%}@%{\e[3m%}%m%{\e[0m%}:%{\e[0;92;49m%}%~%{\e[0m%} %{\e[0;97;49m%}$%{\e[0m%} '
+export PS1='%n@%m:%F{green}%~%f $ '
