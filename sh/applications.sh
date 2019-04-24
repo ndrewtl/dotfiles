28e80edd3d6870399ed1bb52c8f58369053d7cd1
Andrew Lee <andrewl@mbda.fun>
2019-04-24 15:21:14 -0700

    Autodetect editor

--- sh/applications.sh
+++ sh/applications.sh
@@ -1,6 +1,22 @@
 # Applications
+
+# Helper: check if we have the given command
+command_exists() {
+  which $1 > /dev/null 2>&1
+}
+
 export BROWSER=firefox
 export BROWSER2=chromium
-export EDITOR=nvim
 export LAUNCHER='rofi -show run'
 export TERMINAL='kitty --single-instance'
+
+# Use the most modern vi-style editor we can find
+if  command_exists nvim; then
+  export EDITOR=nvim
+elif  command_exists vim; then
+  export EDITOR=vim
+else
+  export EDITOR=vi
+fi
+
+unset command_exists
